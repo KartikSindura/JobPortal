29154161811e0230817449d72c80960959518b6e
KartikSindura <kartik.sindura@gmail.com>
2024-07-24 21:52:38 +0530

    set secure true for cookie

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -87,6 +87,7 @@ export const login = async (req, res) => {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "none",
+        secure: true
       })
       .json({
         message: `Welcome back ${user.fullname}`,
