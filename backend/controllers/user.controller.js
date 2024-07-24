f770dffb2012b6abfb9a70d8ee8865e3222692b9
KartikSindura <kartik.sindura@gmail.com>
2024-07-24 21:48:49 +0530

    set sameSite to none

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -64,7 +64,7 @@ export const login = async (req, res) => {
             role: user.role,
             profile: user.profile
         }
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: false }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none' }).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
